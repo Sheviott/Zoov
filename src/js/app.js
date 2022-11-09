3c2fa8a0b8c681c80adf37bf0afc429f46515a26
Sheviott <game.ru2012@yandex.ru>
2022-11-09 17:53:23 +0300

    fix js error

--- src/js/app.js
+++ src/js/app.js
@@ -8,9 +8,13 @@ import './modules/scroll.js'
 import './modules/dynamicAdapt.js'
 
 document.addEventListener('click', function (e) {
-  if (e.target.classList.contains("plus-btn")) {
+  if(!e.target.classList.contains("plus-btn") && !e.target.classList.contains("minus-btn")) {
+    return
+  }
+  else if (e.target.classList.contains("plus-btn")) {
     ++e.target.parentElement.querySelector("input").value;
-  } else if (e.target.classList.contains("minus-btn") & e.target.parentElement.querySelector("input").value != 1) {
+  }
+  else if (e.target.classList.contains("minus-btn") & e.target.parentElement.querySelector("input").value != 1) {
     --e.target.parentElement.querySelector("input").value;
   }
 });
